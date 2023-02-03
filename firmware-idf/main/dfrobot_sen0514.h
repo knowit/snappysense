3393d07e1b54fbaeb819f521e6737bd334feead0
Lars T Hansen <lars.thomas.hansen@knowit.no>
2023-02-03 12:31:27 +0100

    Stub include

---
+++ firmware-idf/main/dfrobot_sen0514.h
@@ -0,0 +1,11 @@
+/* Driver for air/gas sensor: DFRobot SEN0514.
+ * https://wiki.dfrobot.com/SKU_SEN0514_Gravity_ENS160_Air_Quality_Sensor
+ */
+
+#ifndef dfrobot_sen0514_h_included
+#define dfrobot_sen0514_h_included
+
+#include "main.h"
+
+
+#endif /* !dfrobot_sen0514_h_included */
